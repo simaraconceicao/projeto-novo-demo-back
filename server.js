8a948c7956fce05ab0b3139dfaf74579e4817e58
simaraconceicao <contato@simaraconceicao.com>
2024-02-27 21:21:41 -0300

    insere código

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express') //trazendo funcionalidades do express
+const cors = require('cors') //trazendo funcionalidades do cors
 
-const app = express()
-const PORTA = 3333
-app.use(cors())
+const app = express() // instanciando o express (segunda etapa de configurar o express)
+const PORTA = 3333 // criando a constante Porta pra guardar a informação
+app.use(cors()) //instanciando o cors e terminando a configuração pra liberar a api ser integrada pelo nosso front
 
-app.use(express.json())
+app.use(express.json()) // dizendo que o tipo de dado que minha api permite enviar é JSON
 
-app.get('/ola', function(request, response) {
-  response.json({ message: 'Começou o backend'})
-})
+const router = require('./route')
+app.use('/', router)
 
-app.listen(PORTA, console.log(`Rodando servidor na porta: ${PORTA}`))
+app.listen(PORTA, console.log(`Rodando servidor na porta: ${PORTA}`)) //ligando o servidor
